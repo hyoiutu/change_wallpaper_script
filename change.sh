c6146724b53148a92186571b8d43e12651bf0068
hyoiutu <ke-suke.flandre@ezweb.ne.jp>
2018-01-20 22:45:29 +0900

    縦長用と横長用のディレクトリパスを追加した

--- change.sh
+++ change.sh
@@ -2,9 +2,11 @@
 # when run this script, run a command line the following
 # gconftool-2 --set /apps/nautilus/preferences/show_desktop --type boolean false
 now=$(date +"%H")
+day_time=6
+night_time=20
 
-if [ $now -lt 20 -a $now -gt 6 ]; then
-  feh --randomize --bg-fill ~/wallpaper/dayLight/*
+if [ $now -lt $night_time -a $now -gt $day_time ]; then
+  feh --bg-fill `ls -d ~/wallpaper/dayLight/horizontal/* | shuf -n 1` `ls -d ~/wallpaper/dayLight/vertical/* | shuf -n 1`
 else
-  feh --bg-fill `ls -d ~/wallpaper/night/* | shuf -n 1` `ls -d ~/wallpaper/dayLight/* | shuf -n 1`
+  feh --bg-fill `ls -d ~/wallpaper/night/horizontal/* | shuf -n 1` `ls -d ~/wallpaper/night/vertical/* | shuf -n 1`
 fi
